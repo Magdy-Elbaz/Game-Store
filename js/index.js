b008e6def40a8439e04fdc903e955d74caf9a78a
MagdyElbaz <magdymaamoon17@gmail.com>
2025-11-05 08:54:55 +0200

    update file index js

--- js/index.js
+++ js/index.js
@@ -204,16 +204,16 @@ fetch(`../data.json`).then((re) => re.json()).then((data) => {
                     input_landingPage.value = "";
 
                 } else {
+                    if (input_landingPage.value === "") {
+                        msg_not_product.innerHTML = `<span>!</span> The search field is empty.`;
+                    } else {
+                        msg_not_product.innerHTML = `<span>!</span> The product is not available.`
+                    };
+                    
                     msg_not_product.style.cssText = "scale: 1;";
                     setTimeout(() => {
                         msg_not_product.style.cssText = "scale: 0;";
                     }, 3000);
-                };
-
-                if (input_landingPage.value === "") {
-                    msg_not_product.innerHTML = `<span>!</span> The search field is empty.`;
-                } else {
-                    msg_not_product.innerHTML = `<span>!</span> The product is not available.`
                 };
 
             });
