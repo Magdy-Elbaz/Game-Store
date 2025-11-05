3e6fc6751c5dbd5eb0aefa051dfd484ea0f6f645
MagdyElbaz <magdymaamoon17@gmail.com>
2025-11-05 09:09:27 +0200

    update search

--- js/index.js
+++ js/index.js
@@ -204,16 +204,17 @@ fetch(`../data.json`).then((re) => re.json()).then((data) => {
                     input_landingPage.value = "";
 
                 } else {
-                    if (input_landingPage.value === "") {
-                        msg_not_product.innerHTML = `<span>!</span> The search field is empty.`;
-                    } else if (input_landingPage.value.toLowerCase() !== card.name.toLowerCase() || str.toLowerCase() !== card.name.toLowerCase()) {
-                        msg_not_product.innerHTML = `<span>!</span> The product is not available.`
-                    };
-
                     msg_not_product.style.cssText = "scale: 1;";
                     setTimeout(() => {
                         msg_not_product.style.cssText = "scale: 0;";
                     }, 3000);
+                };
+
+                if (input_landingPage.value === "") {
+                    msg_not_product.innerHTML = `<span>!</span> The search field is empty.`;
+                };
+                if (input_landingPage.value.toLowerCase() !== card.name.toLowerCase() || str.toLowerCase() !== card.name.toLowerCase()) {
+                    msg_not_product.innerHTML = `<span>!</span> The product is not available.`
                 };
 
             });
